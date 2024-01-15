39d144e827493885d062a323516dad8af8ff08ea
YSoyeon <syyang@enki.co.kr>
2024-01-15 21:03:09 +0900

    :bug: fix bug

--- app/[lng]/components/navbar/SignController.tsx
+++ app/[lng]/components/navbar/SignController.tsx
@@ -1,14 +1,18 @@
-import { signIn, signOut, useSession } from "next-auth/react";
-import React from "react";
+import { signIn, signOut, useSession } from 'next-auth/react';
+import React, { useEffect } from 'react';
 
 const SignController = () => {
-	const { data: session } = useSession();
+  const { data: session } = useSession();
 
-	return (
-		<div>
-			{session ? <button onClick={() => signOut()}>로그아웃</button> : <button onClick={() => signIn()}>로그인</button>}
-		</div>
-	);
+  useEffect(() => {
+    console.log(session);
+  }, [session]);
+
+  return (
+    <div>
+      {session ? <button onClick={() => signOut()}>로그아웃</button> : <button onClick={() => signIn()}>로그인</button>}
+    </div>
+  );
 };
 
 export default SignController;
--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -1,17 +1,25 @@
 'use client';
 
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { defaultNS } from '../i18n/settings';
 import { useTranslation } from '../i18n/client';
 
 const Page = ({ params: { lng } }: any) => {
+  const [title, setTitle] = useState('');
+  const [subTitle, setSubTitle] = useState('');
+
   const { t } = useTranslation(lng, defaultNS);
+
+  useEffect(() => {
+    setTitle(t('title'));
+    setSubTitle(t('to-second-page'));
+  }, [t]);
 
   return (
     <>
-      <h1>{t('title')}</h1>
-      <Link href={`/${lng}/second-page`}>{t('to-second-page')}</Link>
+      <h1>{title}</h1>
+      <Link href={`/${lng}/second-page`}>{subTitle}</Link>
     </>
   );
 };
