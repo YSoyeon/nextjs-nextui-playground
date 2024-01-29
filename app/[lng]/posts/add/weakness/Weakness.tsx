1910e1630a4076373ab64612ff0e5f3b0763ffb6
syyang <syyang@enki.co.kr>
2024-01-29 09:07:54 +0900

    feat: cwe selector

--- app/[lng]/posts/add/weakness/Weakness.tsx
+++ app/[lng]/posts/add/weakness/Weakness.tsx
@@ -1,55 +1,87 @@
-'use client';
+"use client";
 
-import React, { useEffect, useState } from 'react';
-import { CardBody, Input, Listbox, ListboxItem, Select, SelectItem } from '@nextui-org/react';
-import { IoMdSearch } from 'react-icons/io';
+import React, { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { CardBody, Input, Listbox, ListboxItem, Select, SelectItem, Selection } from "@nextui-org/react";
+import { IoMdSearch } from "react-icons/io";
 
 async function getCWE() {
-  const res = await fetch('http://localhost:4444/cwe');
-  return res.json();
+	const res = await fetch("http://localhost:4444/cwe");
+	console.log("get CWE");
+	console.log(res);
+	return res.json();
 }
 
 export default function Weakness() {
-  const [clusters, setClusters] = useState<ClusterType[]>([]);
-  const [weaknesses, setWeaknesses] = useState<ClusterType[]>([]);
-  // const { clusters, weaknesses } = await getCWE();
+	const {
+		isLoading,
+		error,
+		data: cwe,
+	} = useQuery({
+		queryKey: ["cwe"],
+		queryFn: getCWE,
+	});
 
-  const getCwe = async () => {
-    const res = await getCWE();
-    setClusters(res.clusters);
-    setWeaknesses(res.weaknesses);
-  };
-  useEffect(() => {
-    getCwe();
-  }, []);
+	console.log("cwe");
+	console.log(cwe);
 
-  console.log('cluster');
-  console.log(clusters);
+	const clusters: ClusterType[] = cwe.clusters;
+	const weaknesses: WeaknessType[] = cwe.weaknesses;
+	const [weakList, setWeakList] = useState<WeaknessType[]>([]);
 
-  return (
-    <CardBody>
-      <div className="border-1">
-        <div className="flex gap-1 items-center">
-          <Input startContent={<IoMdSearch />} />
-          <Select size="md" label="Select Asset Type" className="max-w-xs">
-            {clusters.map((cluster: ClusterType) => (
-              <SelectItem key={cluster.id} value={cluster.name}>
-                {cluster.name}
-              </SelectItem>
-            ))}
-          </Select>
-        </div>
-        <Listbox className="h-[300px] overflow-y-auto	">
-          {weaknesses.map((weakness) => (
-            <ListboxItem key={weakness.id}>{weakness.name}</ListboxItem>
-          ))}
-        </Listbox>
-      </div>
-      <div>
-        <span>
-          Currently selected: <span>(Deselect)</span>
-        </span>
-      </div>
-    </CardBody>
-  );
+	const [value, setValue] = useState<string>("");
+
+	// const { clusters, weaknesses } = await getCWE();
+
+	const getCwe = async () => {
+		const res = await getCWE();
+		// setClusters([{ id: -1, name: "All clusters", description: "" }, ...res.clusters]);
+		// setWeaknesses(res.weaknesses);
+		setWeakList(res.weaknesses);
+	};
+
+	console.log("cluster");
+	console.log(clusters);
+
+	const onSelectionUpdate = (selection: Selection) => {
+		const selectedKey = Number(Array.from(selection)[0]);
+		if (selectedKey === -1) setWeakList(weaknesses);
+		else {
+			const filteredData = weaknesses.filter((value) => value.cluster_ids.some((id) => selectedKey === id));
+			setWeakList(filteredData);
+		}
+	};
+
+	if (isLoading) return "Loading...";
+
+	if (error) return "An error has occurred: " + error.message;
+
+	return (
+		<CardBody>
+			<div className="border-1">
+				<div className="flex gap-1 items-center">
+					<Input startContent={<IoMdSearch />} />
+					<Select size="md" label="Select Asset Type" className="max-w-xs" onSelectionChange={onSelectionUpdate}>
+						{clusters.map((cluster: ClusterType) => (
+							<SelectItem key={cluster.id} value={cluster.name}>
+								{cluster.name}
+							</SelectItem>
+						))}
+					</Select>
+				</div>
+				<Listbox className="h-[300px] overflow-y-auto	">
+					{weakList.map((weakness) => (
+						<ListboxItem key={weakness.id}>
+							{weakness.name}({weakness.external_id})
+						</ListboxItem>
+					))}
+				</Listbox>
+			</div>
+			<div>
+				<span>
+					Currently selected: <span>(Deselect)</span>
+				</span>
+			</div>
+		</CardBody>
+	);
 }
--- app/[lng]/providers.tsx
+++ app/[lng]/providers.tsx
@@ -1,22 +1,27 @@
-'use client';
+"use client";
 
-import { NextUIProvider } from '@nextui-org/react';
-import { useRouter } from 'next/navigation';
-import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import { SessionProvider } from 'next-auth/react';
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { NextUIProvider } from "@nextui-org/react";
+import { useRouter } from "next/navigation";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { SessionProvider } from "next-auth/react";
+import { useState } from "react";
 
 const Providers = ({ children }: any) => {
-  const router = useRouter();
+	const router = useRouter();
+	const client = new QueryClient();
 
-  return (
-    <SessionProvider>
-      <NextUIProvider navigate={router.push}>
-        <NextThemesProvider attribute="class" defaultTheme="dark">
-          {children}
-        </NextThemesProvider>
-      </NextUIProvider>
-    </SessionProvider>
-  );
+	return (
+		<QueryClientProvider client={client}>
+			<SessionProvider>
+				<NextUIProvider navigate={router.push}>
+					<NextThemesProvider attribute="class" defaultTheme="dark">
+						{children}
+					</NextThemesProvider>
+				</NextUIProvider>
+			</SessionProvider>
+		</QueryClientProvider>
+	);
 };
 
 export default Providers;
